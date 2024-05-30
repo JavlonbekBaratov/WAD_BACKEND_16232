abde16beba06d3b73932a42846c1b9126c4ef1a3
Javlonbek Baratov <JavlonbekBaratov912@gmail.com>
2024-05-30 11:01:34 +0500

    Models have been created

---
+++ WAD_BACKEND_16232/Models/Key.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WAD_BACKEND_16232.Models
+{
+    public class Key
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Key name is required")]
+        public string KeyName { get; set; }
+
+        public string KeyInstruction { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Price must be a positive value")]
+        public int Price { get; set; }
+
+        [Required(ErrorMessage = "Manufacturer is required")]
+        public string Manufacturer { get; set; }
+
+        public int? KeyCategoryID { get; set; }
+
+        [ForeignKey("KeyCategoryID")]
+        public KeyCategory? KeyCategory { get; set; }
+    }
+}
---
+++ WAD_BACKEND_16232/Models/KeyCategory.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace WAD_BACKEND_16232.Models
+{
+    public class KeyCategory
+    {
+        [Key]
+        public int KeyCategoryID { get; set; }
+
+        [Required(ErrorMessage = "Key category name is required")]
+        public string KeyCategoryName { get; set; }
+
+        public string KeyDescription { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public SecurityLevel SecurityLevel { get; set; }
+    }
+
+    public enum SecurityLevel
+    {
+        high,
+        medium,
+        low
+    }
+}
